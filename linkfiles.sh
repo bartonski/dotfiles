5ad08156f794551a58c3febfc587128fd427f5b6
Barton Chittenden <barton@bywatersolutions.com>
2015-04-13 08:24:17 -0400

    Add .xinitrc, minor changes to .alias, .tmux.conf, bin/daystart, bin/todo, linkfiles.sh

--- linkfiles.sh
+++ linkfiles.sh
@@ -1,7 +1,22 @@
 #! /bin/sh
 
 cd $HOME
-for dotfile in $(find User/ -maxdepth 1 -type f -name '.*' ); do ln -s $(realpath $dotfile) $HOME; done
+
+DOTFILES_BACKUP="$HOME/.user_dotfiles_backup"
+
+if [ ! -d $DOTFILES_BACKUP ]
+then
+    mkdir $DOTFILES_BACKUP
+fi
+for dotfile in $(find User/ -maxdepth 1 -type f -name '.*' )
+do 
+    BASE=$(basename $dotfile)
+    if [ -e "$BASE" ]
+    then
+        mv "$BASE" $DOTFILES_BACKUP
+    fi
+    ln -s $(realpath $dotfile) $HOME
+done
 
 if [ ! -d ./bin ]
 then
