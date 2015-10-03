2c4ecf2e21d8de5f6f0eec16f34f3a99ad62bd35
Barton Chittenden <barton@bywatersolutions.com>
2015-10-03 15:50:13 -0400

    Remove dependenency on realpath in linkfiles.sh

--- linkfiles.sh
+++ linkfiles.sh
@@ -8,14 +8,22 @@ if [ ! -d $DOTFILES_BACKUP ]
 then
     mkdir $DOTFILES_BACKUP
 fi
-for dotfile in $(find User/ -maxdepth 1 -type f -name '.*' )
+for dotfile in $(find $HOME/User/ -maxdepth 1 -type f -name '.*' )
 do 
     BASE=$(basename $dotfile)
+
+    # file is a symbolic link, go ahead and remove it.
+    if [ -h "$BASE" ]
+    then
+        rm "$BASE" 
+    fi
+
+    # back up old files to $DOTFILES_BACKUP
     if [ -e "$BASE" ]
     then
         mv "$BASE" $DOTFILES_BACKUP
     fi
-    ln -s $(realpath $dotfile) $HOME
+    ln -s $dotfile $HOME
 done
 
 if [ ! -d ./bin ]
